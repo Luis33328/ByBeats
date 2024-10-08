e8874c7501667179c1069573153e4dbe822b50a0
João Xavier <joao.xavier@driva.com.br>
2024-10-07 22:32:54 -0300

    order by options and pagination

--- bybeatsfront/src/app/pages/beats/components/pesquisar/pesquisar.component.ts
+++ bybeatsfront/src/app/pages/beats/components/pesquisar/pesquisar.component.ts
@@ -3,7 +3,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ConverterUtils } from 'src/app/common/converter.utils';
-import {NgxTinySliderSettingsInterface, NgxTinySliderComponent} from 'ngx-tiny-slider';
+import { PageEvent } from '@angular/material/paginator';
 import { BeatService } from '../../service/beat.service';
 
 @Component({
@@ -17,6 +17,15 @@ export class PesquisarBeatComponent implements OnInit {
 
   public beats = [];
   private searchTerm: string = '';
+  private lastSortedProperty: string = '';
+  private sortDirection: number = 1;
+  private originalOrders = {};
+  private lastSortedDirections = {};
+  public pageSize = 12;
+  public pageSizeOptions: number[] = [5, 10, 25, 100];
+  public pageEvent: PageEvent;
+  public displayedBeats = [];
+
 
   constructor(
     private router: Router,
@@ -28,26 +37,57 @@ export class PesquisarBeatComponent implements OnInit {
     this.getBeats()
   }
 
-  private getBeats(){
+  public getBeats(){
     this.beatService.listWithoutPaginator().subscribe(data => {
       this.beats = data;
+      this.displayedBeats = data.slice(0, this.pageSize); 
       console.log(data);
     }, err => {
       console.log("List Error.")
     });
   }
 
-
   public view(event) {
     this.router.navigate(['/beat/' + event]);
   }
-
 
   public getId(row) {
     console.log(row);
     this.view(row.guidBeat);
   }
+  public paginate(event: PageEvent) {
+    const start = event.pageIndex * event.pageSize;
+    const end = start + event.pageSize;
+    this.displayedBeats = this.beats.slice(start, end);
+  
+    this.pageEvent.pageIndex = 0;
+  }
 
-
+  public orderBy(property: string) {
+    if (!this.originalOrders[property]) {
+      this.originalOrders[property] = [...this.beats];
+    }
+  
+    if (this.lastSortedProperty === property) {
+      if (this.lastSortedDirections[property] === 1) {
+        this.beats.sort((a, b) => b[property].localeCompare(a[property]));
+        this.lastSortedDirections[property] = -1;
+      } else if (this.lastSortedDirections[property] === -1) {
+        this.beats = [...this.originalOrders[property]];
+        this.lastSortedDirections[property] = 0;
+      } else {
+        this.beats.sort((a, b) => a[property].localeCompare(b[property]));
+        this.lastSortedDirections[property] = 1;
+      }
+    } else {
+      this.beats.sort((a, b) => a[property].localeCompare(b[property]));
+      this.lastSortedProperty = property;
+      this.lastSortedDirections[property] = 1;
+    }
+  
+    // Reset the pagination to the first page
+    this.pageEvent.pageIndex = 0;
+    this.displayedBeats = this.beats.slice(0, this.pageSize);
+  }
 
 }
