6b5e029770d8c0c8b207c13bf1660d9e44fa354a
João Xavier <joao.xavier@driva.com.br>
2024-11-03 15:17:07 -0300

    correção filtros + dashboard organizado

--- bybeatsfront/src/app/pages/beats/components/pesquisar/pesquisar.component.ts
+++ bybeatsfront/src/app/pages/beats/components/pesquisar/pesquisar.component.ts
@@ -16,8 +16,8 @@ export class PesquisarBeatComponent implements OnInit {
   public form: FormGroup = new FormGroup({});
 
   public beats = [];
-  public bpmMin = 80;
-  public bpmMax = 120;
+  public bpmMin = 24;
+  public bpmMax = 300;
   private searchTerm: string = '';
   private lastSortedProperty: string = '';
   private sortDirection: number = 1;
@@ -28,7 +28,6 @@ export class PesquisarBeatComponent implements OnInit {
   public pageEvent: PageEvent;
   public displayedBeats = [];
 
-
   constructor(
     private router: Router,
     private activeRoute: ActivatedRoute,
@@ -36,7 +35,7 @@ export class PesquisarBeatComponent implements OnInit {
     private snackBar: MatSnackBar) { }
 
   public ngOnInit() {
-    this.getBeats()
+    this.getBeats();
   }
 
   public getBeats() {
@@ -45,7 +44,7 @@ export class PesquisarBeatComponent implements OnInit {
       this.displayedBeats = this.beats.slice(0, this.pageSize); 
       console.log(data);
     }, err => {
-      console.log("List Error.")
+      console.log("List Error.");
     });
   }
 
@@ -57,12 +56,12 @@ export class PesquisarBeatComponent implements OnInit {
     console.log(row);
     this.view(row.guidBeat);
   }
+
   public paginate(event: PageEvent) {
     const start = event.pageIndex * event.pageSize;
     const end = start + event.pageSize;
     this.displayedBeats = this.beats.slice(start, end);
-  
-    this.pageEvent.pageIndex = 0;
+    this.pageEvent = event;
   }
 
   public orderBy(property: string) {
@@ -72,24 +71,55 @@ export class PesquisarBeatComponent implements OnInit {
   
     if (this.lastSortedProperty === property) {
       if (this.lastSortedDirections[property] === 1) {
-        this.beats.sort((a, b) => b[property].localeCompare(a[property]));
+        this.beats.sort((a, b) => {
+          if (typeof a[property] === 'number' && typeof b[property] === 'number') {
+            return b[property] - a[property];
+          }
+          return b[property].localeCompare(a[property]);
+        });
         this.lastSortedDirections[property] = -1;
       } else if (this.lastSortedDirections[property] === -1) {
         this.beats = [...this.originalOrders[property]];
         this.lastSortedDirections[property] = 0;
       } else {
-        this.beats.sort((a, b) => a[property].localeCompare(b[property]));
+        this.beats.sort((a, b) => {
+          if (typeof a[property] === 'number' && typeof b[property] === 'number') {
+            return a[property] - b[property];
+          }
+          return a[property].localeCompare(b[property]);
+        });
         this.lastSortedDirections[property] = 1;
       }
     } else {
-      this.beats.sort((a, b) => a[property].localeCompare(b[property]));
+      this.beats.sort((a, b) => {
+        if (typeof a[property] === 'number' && typeof b[property] === 'number') {
+          return a[property] - b[property];
+        }
+        return a[property].localeCompare(b[property]);
+      });
       this.lastSortedProperty = property;
       this.lastSortedDirections[property] = 1;
     }
   
-    // Reset the pagination to the first page
-    this.pageEvent.pageIndex = 0;
+    this.pageEvent = { pageIndex: 0, pageSize: this.pageSize, length: this.beats.length };
     this.displayedBeats = this.beats.slice(0, this.pageSize);
   }
 
+  public onBpmMinChange(value: number) {
+    if (value > this.bpmMax) {
+      this.bpmMin = this.bpmMax;
+    } else {
+      this.bpmMin = value;
+    }
+    this.getBeats();
+  }
+
+  public onBpmMaxChange(value: number) {
+    if (value < this.bpmMin) {
+      this.bpmMax = this.bpmMin;
+    } else {
+      this.bpmMax = value;
+    }
+    this.getBeats();
+  }
 }
