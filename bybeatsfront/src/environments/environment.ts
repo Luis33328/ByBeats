3a420542e96e1460f30c38c7ca1d0e124e1234f3
João Xavier <joao.xavier@driva.com.br>
2024-11-07 22:11:17 -0300

    (ignore branch name, s3 was used instead)
    débito técnico que chama

--- bybeatsfront/src/environments/environment.ts
+++ bybeatsfront/src/environments/environment.ts
@@ -2,17 +2,6 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false,
-  baseUrl: 'http://localhost:8080',
-  apiKey: "AIzaSyDkuP7jDXqtZS2t2Fqd_zuvA-B16GABGkc",
-  authDomain: "bybeats-a9065.firebaseapp.com",
-  projectId: "bybeats-a9065",
-  storageBucket: "bybeats-a9065.appspot.com",
-  messagingSenderId: "264142330207",
-  appId: "1:264142330207:web:a30651594182dc364afeea"
-};
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
